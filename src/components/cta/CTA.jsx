78f47710bad41b89832a2fea15ac5a691640819b
Shadrack Muwanguzi <shadrackmuwanguzi@Shadracks-MacBook-Pro.local>
2023-01-24 15:19:46 +0300

    New update

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -3,8 +3,14 @@ import './cta.css'
 
 const CTA = () => {
   return (
-    <div>
-      CTA
+    <div className="gtp3__cta">
+      <div className='gtp3__cta-content'>
+        <p>Request Early Access to Get Started</p>
+        <h3>Register today & start exploring the endless possiblities.</h3>
+      </div>
+      <div className='gtp3__cta-btn'>
+        <button type="button">Get Started</button>
+      </div>
     </div>
   )
 }
